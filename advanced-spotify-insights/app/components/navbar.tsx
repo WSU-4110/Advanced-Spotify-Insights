934085db3d21e695fc628d6c75afaab77da618ef
kevdagg <kevinhin4972@gmail.com>
2026-02-16 19:51:30 -0500

    Refactor Navbar component and enhance Feedback page layout with form inputs

--- advanced-spotify-insights/app/components/navbar.tsx
+++ advanced-spotify-insights/app/components/navbar.tsx
@@ -1,18 +1,37 @@
 import Link from "next/link";
 
 export default function Navbar() {
+  const navLinks = [
+    { name: "Home", href: "/" },
+    { name: "Feedback", href: "/feedback" },
+    { name: "Login", href: "/login" },
+  ];
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <div className="w-full max-w-md rounded-2xl bg-white p-8 shadow-lg dark:bg-zinc-900">
-        <Link
-          className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-          href="/" //Leads to homepage
-          target=""
-          rel="noopener noreferrer"
-        >
-          Home
-        </Link>
+    <nav className="sticky top-0 border-b border-gray-800 z-50 w-full">
+      {/* main wrapper*/}
+      <div className="mx-auto flex h-16 items-center justify-between px-4 px-6">
+        <div className="flex flex-shrink-0 items-center">
+          {" "}
+          {/* brand wrapper */}
+          <Link href="/" className="text-xl font-bold">
+            Advanced Spotify Insights
+          </Link>
+        </div>
+
+        <div className="flex items-baseline">
+          {/* nav links wrapper*/}
+          {navLinks.map((link) => (
+            <Link
+              key={link.name}
+              href={link.href}
+              className="rounded-md px-3 py-2 text-sm font-medium dark:hover:bg-gray-800"
+            >
+              {link.name}
+            </Link>
+          ))}
+        </div>
       </div>
-    </div>
+    </nav>
   );
 }
