179f02d439479b56b5bd16cf3dfaf63258108296
kevdagg <kevinhin4972@gmail.com>
2026-03-16 18:20:54 -0400

    Extend NextAuth session and JWT interfaces to include accessToken

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -23,7 +23,7 @@ export const authOptions: NextAuthOptions = {
       return token;
     },
     async session({ session, token }) {
-      session.accessToken = token.accessToken as string; 
+      session.accessToken = token.accessToken as string;
       return session;
     },
   },
